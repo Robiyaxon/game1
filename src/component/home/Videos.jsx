6ae00d5a6fd2d0c320820fccc7a7559c42aabdff
Robiyaxon <tuhtaevarobiahon@gmail.com>
2025-01-29 17:11:19 +0500

    @fixed@

--- src/component/home/Videos.jsx
+++ src/component/home/Videos.jsx
@@ -12,7 +12,7 @@ const Videos = () => {
                     <a href="https://youtu.be/wJECu_mPBEk" target="_blank" rel="noopener noreferrer">
                         <img src={img1} alt="" />
                         <p>1-dars Sezish o'rganlari</p>
-                        <h4>18:30 daqiqa</h4>
+                        <h4>2 daqiqa</h4>
                     </a>
 
                 </div>
@@ -20,7 +20,7 @@ const Videos = () => {
                     <a href="https://youtu.be/uspeilWB7o0" target="_blank" rel="noopener noreferrer">
                         <img src={img2} alt="" />
                         <p>2-dars Hid bilish </p>
-                        <h4>18:30 daqiqa</h4>
+                        <h4>2:13 daqiqa</h4>
                     </a>
 
                 </div>
@@ -28,7 +28,7 @@ const Videos = () => {
                     <a href="https://youtu.be/j_YLc1P7DDc" target="_blank" rel="noopener noreferrer">
                         <img src={img3} alt="" />
                         <p>3-dars Tam bilish</p>
-                        <h4>18:30 daqiqa</h4>
+                        <h4>2:16 daqiqa</h4>
                     </a>
 
                 </div>
@@ -36,7 +36,7 @@ const Videos = () => {
                     <a href="https://youtu.be/2GUD_dsMUF8" target="_blank" rel="noopener noreferrer">
                         <img src={img3} alt="" />
                         <p>4-dars Ko`rish</p>
-                        <h4>18:30 daqiqa</h4>
+                        <h4>2:40 daqiqa</h4>
                     </a>
 
                 </div>
@@ -44,7 +44,7 @@ const Videos = () => {
                     <a href="https://youtu.be/m6UsBR4xOHU" target="_blank" rel="noopener noreferrer">
                         <img src={img3} alt="" />
                         <p>5-dars Eshitish</p>
-                        <h4>18:30 daqiqa</h4>
+                        <h4>2:40 daqiqa</h4>
                     </a>
 
                 </div>
