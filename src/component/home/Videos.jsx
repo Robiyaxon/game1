0e501b340f487d0deb8cfc2c325b2b761c4b2f42
Robiyaxon <tuhtaevarobiahon@gmail.com>
2025-01-29 17:08:27 +0500

    @fixed@

--- src/component/home/Videos.jsx
+++ src/component/home/Videos.jsx
@@ -9,7 +9,7 @@ const Videos = () => {
             <h1>Informatika fanidan videodarsliklar</h1>
             <div className={style.block}>
                 <div className={style.box}>
-                    <a href="https://www.youtube.com/watch?v=-2caC-uI7l4" target="_blank" rel="noopener noreferrer">
+                    <a href="https://youtu.be/wJECu_mPBEk" target="_blank" rel="noopener noreferrer">
                         <img src={img1} alt="" />
                         <p>1-dars Sezish o'rganlari</p>
                         <h4>18:30 daqiqa</h4>
@@ -17,17 +17,33 @@ const Videos = () => {
 
                 </div>
                 <div className={style.box}>
-                    <a href="https://www.youtube.com/watch?v=-2caC-uI7l4" target="_blank" rel="noopener noreferrer">
+                    <a href="https://youtu.be/uspeilWB7o0" target="_blank" rel="noopener noreferrer">
                         <img src={img2} alt="" />
-                        <p>2-dars Sezish o'rganlari</p>
+                        <p>2-dars Hid bilish </p>
                         <h4>18:30 daqiqa</h4>
                     </a>
 
                 </div>
                 <div className={style.box}>
-                    <a href="https://www.youtube.com/watch?v=-2caC-uI7l4" target="_blank" rel="noopener noreferrer">
+                    <a href="https://youtu.be/j_YLc1P7DDc" target="_blank" rel="noopener noreferrer">
                         <img src={img3} alt="" />
-                        <p>3-dars Sezish o'rganlari</p>
+                        <p>3-dars Tam bilish</p>
+                        <h4>18:30 daqiqa</h4>
+                    </a>
+
+                </div>
+                <div className={style.box}>
+                    <a href="https://youtu.be/2GUD_dsMUF8" target="_blank" rel="noopener noreferrer">
+                        <img src={img3} alt="" />
+                        <p>4-dars Ko`rish</p>
+                        <h4>18:30 daqiqa</h4>
+                    </a>
+
+                </div>
+                <div className={style.box}>
+                    <a href="https://youtu.be/m6UsBR4xOHU" target="_blank" rel="noopener noreferrer">
+                        <img src={img3} alt="" />
+                        <p>5-dars Eshitish</p>
                         <h4>18:30 daqiqa</h4>
                     </a>
 
