44cf5cb448f0c0e6da52de9325226bda2378f22a
Robiyaxon <tuhtaevarobiahon@gmail.com>
2025-07-17 17:15:21 +0500

    fixed

--- src/component/home/Videos.jsx
+++ src/component/home/Videos.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'; // react-router-dom import qilish
-import style from "./Videos.module.css"
-import img1 from "../../assert/aaa.png"
-import img2 from "../../assert/cc.png"
-import img3 from "../../assert/gg.png"
+import React from 'react';
+import style from "./Videos.module.css";
+import img1 from "../../assert/aaa.png";
+import img2 from "../../assert/cc.png";
+import img3 from "../../assert/gg.png";
 
 const Videos = () => {
     return (
@@ -11,43 +10,64 @@ const Videos = () => {
             <h1>Informatika fanidan videodarsliklar</h1>
             <div className={style.block}>
                 <div className={style.box}>
-                    <Link to="/home/video/1">  {/* video bo'yicha yo'naltirish */}
-                        <img src={img1} alt="" />
-                        <p>1-dars Sezish o'rganlari</p>
+                    <a href="https://game1-pearl.vercel.app/static/media/aaa.de1cfe607d38de327183.png" target="_blank" rel="noopener noreferrer">
+                        <img src={img1} alt="1-dars" />
+                        <p>1-dars Sezgi o‘rganlari</p>
                         <h4>2 daqiqa</h4>
-                    </Link>
+                    </a>
                 </div>
                 <div className={style.box}>
-                    <Link to="/home/video/2">
-                        <img src={img2} alt="" />
-                        <p>2-dars Hid bilish</p>
+                    <a href="https://game1-pearl.vercel.app/home/video/4" target="_blank" rel="noopener noreferrer">
+                        <img src={img2} alt="2-dars" />
+                        <p>2-dars Biz nimani ko‘ramiz?</p>
+                        <h4>2:40 daqiqa</h4>
+                    </a>
+                </div>
+                <div className={style.box}>
+                    <a href="https://youtu.be/bEDlvOtHNRg" target="_blank" rel="noopener noreferrer">
+                        <img src={img3} alt="3-dars" />
+                        <p>3-dars Biz olamni qanday qabul qilamiz</p>
+                        <h4>3:20 daqiqa</h4>
+                    </a>
+                </div>
+                <div className={style.box}>
+                    <a href="https://game1-pearl.vercel.app/home/video/5" target="_blank" rel="noopener noreferrer">
+                        <img src={img3} alt="4-dars" />
+                        <p>4-dars Biz nimani eshitamiz?</p>
+                        <h4>2:40 daqiqa</h4>
+                    </a>
+                </div>
+                <div className={style.box}>
+                    <a href="https://game1-pearl.vercel.app/home/video/2" target="_blank" rel="noopener noreferrer">
+                        <img src={img2} alt="5-dars" />
+                        <p>5-dars Hid bilish</p>
                         <h4>2:13 daqiqa</h4>
-                    </Link>
+                    </a>
                 </div>
                 <div className={style.box}>
-                    <Link to="/home/video/3">
-                        <img src={img3} alt="" />
-                        <p>3-dars Tam bilish</p>
+                    <a href="https://game1-pearl.vercel.app/home/video/3" target="_blank" rel="noopener noreferrer">
+                        <img src={img3} alt="6-dars" />
+                        <p>6-dars Ta’m sezish</p>
                         <h4>2:16 daqiqa</h4>
-                    </Link>
+                    </a>
                 </div>
                 <div className={style.box}>
-                    <Link to="/home/video/4">
-                        <img src={img3} alt="" />
-                        <p>4-dars Ko`rish</p>
-                        <h4>2:40 daqiqa</h4>
-                    </Link>
+                    <a href="https://youtu.be/CXKj7bm4Ops" target="_blank" rel="noopener noreferrer">
+                        <img src={img2} alt="7-dars" />
+                        <p>7-dars His qilish</p>
+                        <h4>3 daqiqa</h4>
+                    </a>
                 </div>
                 <div className={style.box}>
-                    <Link to="/home/video/5">
-                        <img src={img3} alt="" />
-                        <p>5-dars Eshitish</p>
-                        <h4>2:40 daqiqa</h4>
-                    </Link>
+                    <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                        <img src={img3} alt="8-dars" />
+                        <p>8-dars Buyumlarga ta’rif beramiz</p>
+                        <h4>3 daqiqa</h4>
+                    </a>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Videos;
